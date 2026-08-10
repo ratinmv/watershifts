ffb0312b1646482946e5c35372d6b4c9e206c77e
ratinmv <ratin.gaming2025@gmail.com>
2026-08-10 21:18:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -210,7 +210,9 @@ class WatershiftsApp {
         return `
             <div class="page-container">
                 <div class="about-container">
-                    <div class="about-image"><img url="https://yt3.googleusercontent.com/PrBzqWYlPMkbMHUxAN0qA_ApRol9tZSAbeINewTRSlpB-NcDl4pkD8ZcQ1zDXnfv1TNDbvZraw=s160-c-k-c0x00ffffff-no-rj"><img></div>
+                    <div class="about-image">
+                        <img src="https://yt3.googleusercontent.com/PrBzqWYlPMkbMHUxAN0qA_ApRol9tZSAbeINewTRSlpB-NcDl4pkD8ZcQ1zDXnfv1TNDbvZraw=s160-c-k-c0x00ffffff-no-rj" alt="Profile photo">
+                    </div>
                     <div class="about-text">
                         <h2>Dr. Nafiseh Salehi Siavashani</h2>
                         <p><strong>Hydrogeologist & Researcher</strong></p>
